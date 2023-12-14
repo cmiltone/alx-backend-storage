327453cc01eff4f5bac5bf23c226a8f128f1ddd8
Castro Hamiltone <castromiltone@gmail.com>
2023-12-15 00:15:32 +0300

    fix

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,5 +1,5 @@
 -- script creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
-CREATE TEMP FUNCTION SafeDiv(a INT, b INT)
+CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
 BEGIN
 DECLARE return_value FLOAT
@@ -9,4 +9,4 @@ END
 ELSE
 SET return_value = a / b;
 END
-RETURN return_value
+RETURN return_value;
