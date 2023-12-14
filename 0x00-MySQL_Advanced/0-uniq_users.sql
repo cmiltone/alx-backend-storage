b662bb6037bfe50c11964b5d3bfeaf96ab330fbe
Castro Hamiltone <castromiltone@gmail.com>
2023-12-14 20:29:47 +0300

    0-uniq_users.sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+--- script creates users table
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  email VARCHAR(255),
+  name VARCHAR(255),
+);
