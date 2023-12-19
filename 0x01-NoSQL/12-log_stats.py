a8f5bceb61c73b60f9a04a05cf44f7e4ecd74fc7
Castro Hamiltone <castromiltone@gmail.com>
2023-12-19 18:53:22 +0300

    fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -37,6 +37,6 @@ def print_logs(collection):
 if __name__ == '__main__':
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
-    print('{} logs'.format(client.count_documents({})))
+    print('{} logs'.format(collection.count_documents({})))
 
     print_logs(collection)
