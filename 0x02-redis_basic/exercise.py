cceef1bb623031891b9ef467859a410e6f44b23a
Castro Hamiltone <castromiltone@gmail.com>
2023-12-20 23:23:18 +0300

    fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -52,6 +52,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb(True)
 
+    @call_history
     @count_calls
     def store(self, data: Data) -> str:
         """stores data to redis"""
