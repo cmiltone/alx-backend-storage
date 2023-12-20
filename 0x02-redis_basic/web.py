3687c401c177d18234fafed6e39e69def115fbc2
Castro Hamiltone <castromiltone@gmail.com>
2023-12-21 00:17:49 +0300

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,53 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""
+In this tasks, we will implement a get_page
+function (prototype: def get_page(url: str) -> str:).
+The core of the function is very simple. It uses the requests
+module to obtain the HTML content of a particular URL and returns it.
+
+Start in a new file named web.py and do not reuse the code written in
+exercise.py.
+
+Inside get_page track how many times a particular URL was accessed in
+the key "count:{url}" and cache the result with an expiration time of
+10 seconds.
+
+Tip: Use http://slowwly.robertomurray.co.uk to simulate a slow response
+and test your caching.
+
+Bonus: implement this use case with decorators.
+"""
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
-
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+store = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def cache_deco(method: Callable) -> Callable:
+    """decorator"""
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+    def caller(url) -> str:
+        """calls the method"""
+        store.incr(f'count:{url}')
+
+        return_value = store.get(f'value:{url}')
+
+        if return_value is not None:
+            return return_value.decode('utf-8')
+
+        return_value = method(url)
+
+        store.set(f'count:{url}', 0)
+
+        store.setex(f'value:{url}', 10, return_value)
+
+        return return_value
+    return caller
 
 
-@data_cacher
+@cache_deco
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
+    """gets HTML from url"""
     return requests.get(url).text
