7d130265097b07f498cc0104fd858e86222b9138
Castro Hamiltone <castromiltone@gmail.com>
2023-12-20 22:06:32 +0300

    fix

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -28,7 +28,7 @@ class Cache:
     def store(self, data: Data) -> str:
         """stores data to redis"""
         key = str(uuid.uuid4())
-        self._redis.mset({key: data})
+        self._redis.set({key: data})
         return key
 
     def get(self, key: str, fn: Callable) -> Data:
@@ -46,9 +46,9 @@ class Cache:
         back to the desired format.
         """
         if fn is None:
-            return self._redis.mget(key)
+            return self._redis.get(key)
         else:
-            return fn(self._redis.mget(key))
+            return fn(self._redis.get(key))
 
     def get_str(self, key: str) -> str:
         """parametizes get"""
